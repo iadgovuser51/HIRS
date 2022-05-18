21b3ef6819c28fe56fe287f2fc690a0d70a1a0eb
iadgovuser51 <iadgovuser@gmail.com>
2022-05-18 13:00:55 -0400

    Ran out of disk space for VM. Saving work to migrate work

--- package/flatpak-aca/scripts/aca-build.sh
+++ package/flatpak-aca/scripts/aca-build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Strip SSL for db connection string
+# TODO: Remove once tomcat is updated to match new ssl settings in connector
+DB_URL="persistence.db.url"
+NO_SSL_DB_URL="persistence.db.url                  = jdbc:mariadb://localhost/hirs_db?autoReconnect=true"
+
+#BEFORE
+cat HIRS_Utils/src/main/resources/persistence.properties
+
+sed -i "\@${DB_URL}@c${NO_SSL_DB_URL}" HIRS_Utils/src/main/resources/persistence.properties
+
+# CHECK
+cat HIRS_Utils/src/main/resources/persistence.properties
+
 PATH=/app/jre/bin:/usr/bin:/app/lib:/app/bin:/app/mariadb/bin
-PROTOC_HOME=/app/bin
-JAVA_HOME=/app/jre
 ./gradlew :HIRS_AttestationCA:clean :HIRS_AttestationCA:build :HIRS_AttestationCAPortal:clean :HIRS_AttestationCAPortal:build
